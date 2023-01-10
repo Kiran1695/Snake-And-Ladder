62283bcf15141f3a37d638a2f2a9518c82c8a556
Kiran1695 <hindlekarkiran.kh@gmail.com>
2023-01-11 00:13:35 +0530

    Welcome Player

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,5 +3,9 @@ package com.bridgelabz;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome Player");
+        // int position =0;
+        System.out.println("Player Rolls The Die");
+        int randomv = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        System.out.println(" Number Got " + randomv);
     }
 }
